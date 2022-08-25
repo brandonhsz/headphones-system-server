b4929c7e399c7de07b4ffcad767dbd62c28ef507
brandonhz <brandonhszzz@gmail.com>
2022-08-25 18:45:57 -0500

    Clean src directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DiademasModule } from './diademas/diademas.module';
 import { UsersModule } from './users/users.module';
 
@@ -15,7 +13,7 @@ import { UsersModule } from './users/users.module';
     DiademasModule,
     UsersModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { APP_FILTER, NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   app.use(LoggerMiddleware);
   await app.listen(process.env.PORT_SERVER || 3001);
 }
